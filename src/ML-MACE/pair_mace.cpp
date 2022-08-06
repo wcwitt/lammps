8f8f64a5a77f9fb84784250357450a4180e30fa9
Chuck Witt <witt.chuck@gmail.com>
2022-08-06 13:22:51 +0100

    add minimal pair_mace (only the required pure virtual functions).

---
+++ src/ML-MACE/pair_mace.cpp
@@ -0,0 +1,51 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributors
+      William C Witt (University of Cambridge)
+------------------------------------------------------------------------- */
+
+#include "pair_mace.h"
+
+using namespace LAMMPS_NS;
+
+/* ---------------------------------------------------------------------- */
+
+PairMACE::PairMACE(LAMMPS *lmp) : Pair(lmp)
+{
+}
+
+/* ---------------------------------------------------------------------- */
+
+PairMACE::~PairMACE()
+{
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairMACE::compute(int eflag, int vflag)
+{
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairMACE::settings(int narg, char **arg)
+{
+}
+
+/* ---------------------------------------------------------------------- */
+
+void PairMACE::coeff(int narg, char **arg)
+{
+}
---
+++ src/ML-MACE/pair_mace.h
@@ -0,0 +1,48 @@
+/* -*- c++ -*- ----------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://www.lammps.org/, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributors
+      William C Witt (University of Cambridge)
+------------------------------------------------------------------------- */
+
+#ifdef PAIR_CLASS
+// clang-format off
+PairStyle(mace,PairMACE);
+// clang-format on
+#else
+
+#ifndef LMP_PAIR_MACE_H
+#define LMP_PAIR_MACE_H
+
+#include "pair.h"
+
+namespace LAMMPS_NS {
+
+class PairMACE : public Pair {
+
+ public:
+
+  PairMACE(class LAMMPS *);
+  ~PairMACE() override;
+  void compute(int, int) override;
+  void settings(int, char **) override;
+  void coeff(int, char **) override;
+
+ protected:
+
+};
+}    // namespace LAMMPS_NS
+
+#endif
+#endif
